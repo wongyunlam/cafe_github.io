d4344644c684d35223286d1ba41ed9261dfe3393
wongyunlam <66591276+wongyunlam@users.noreply.github.com>
2021-07-12 17:46:17 +0800

    Add files via upload

--- set.js
+++ set.js
@@ -9,8 +9,6 @@ document
     }
   })
 
-
-
 function closest(node, className) {
   while(node && node.classList) {
     if (node.classList.contains(className)) {
@@ -19,3 +17,7 @@ function closest(node, className) {
     node = node.parentNode
   }
 } 
+
+if (screen.width < 768) {
+  document.getElementById('myLink').setAttribute('href', "https://mypaper.m.pchome.com.tw/windcat13n/category/");
+}
