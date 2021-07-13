d46cf1d60ff54bb4ea5777989b8d3621e2772a6c
wongyunlam <66591276+wongyunlam@users.noreply.github.com>
2021-07-14 00:39:10 +0800

    Add files via upload

--- set.js
+++ set.js
@@ -9,6 +9,19 @@ document
     }
   })
 
+document
+.querySelector('.faq-block')
+.addEventListener('click', function(e) {
+  console.log(e.target.classList.value)
+  const element = closest(e.target, 'faq-item1')
+  if (element) {
+    console.log(element)
+    element.classList.toggle('Q')
+  }
+})
+
+
+
 function closest(node, className) {
   while(node && node.classList) {
     if (node.classList.contains(className)) {
